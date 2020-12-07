3ca25251a445deb7d3cc78d9729b856f27f52ea5
Jacob Van Cleave <vancleave.jacob@gmail.com>
2020-12-07 10:14:25 -0800

    Initialize repository

---
+++ lib/jvc_palindrome.rb
@@ -0,0 +1,6 @@
+require "jvc_palindrome/version"
+
+module JvcPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jvc_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JvcPalindrome
+  VERSION = "0.1.0"
+end
